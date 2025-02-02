f0c4d52b1d74e270e1612d888c13fff0ae7f68a0
mrnithesh <mr.nithesh.k@gmail.com>
2025-02-02 20:08:27 +0530

    finding-the-percentage

---
+++ finding-the-percentage.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    marks=student_marks[query_name]
+    sum=0
+    for i in marks:
+        sum+=i
+    avg=sum/len(marks)
+    print(f"{avg:.2f}")
