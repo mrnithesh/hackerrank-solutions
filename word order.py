44235470e639eb8ddcf3207b4c98bc18ef577ea1
Nithesh K <mr.nithesh.k@gmail.com>
2025-02-09 06:46:45 +0000

    word-count-counter-approach

--- word order.py
+++ word order.py
@@ -1,4 +1,8 @@
+from collections import Counter
 if __name__=="__main__":
+    """
+    #dictionary key:value based approach
+
     N=int(input())
     words={}
     for _ in range(N):
@@ -6,3 +10,15 @@ if __name__=="__main__":
         words[word]=words.get(word,0)+1
     print(len(words))
     print(*words.values())
+    
+    
+    """
+    #counter based approach
+    
+    N=int(input())
+    words=[]
+    for _ in range(N):
+        words.append(input())
+    count=Counter(words)
+    print(len(count))
+    print(*count.values())
